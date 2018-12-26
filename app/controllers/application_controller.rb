d39658807d0131487569bab99089ab074fdfcdb4
Thang6797 <thangnguyenquoc6797@gmail.com>
2018-12-26 14:19:32 +0700

    Chapter13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t("controllers.application_controller.errorlogin")
+    redirect_to login_url
+  end
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  def account_activation user
+    @user = user
+    mail to: user.email, subject: t("mailers.user_mailer.account_activation")
+  end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("mailers.user_mailer.reset_password")
+  end
+end
