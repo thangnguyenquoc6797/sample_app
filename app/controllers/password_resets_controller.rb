44c912820d98eaa02ea3866043a8587b8f22fdf9
Thang6797 <thangnguyenquoc6797@gmail.com>
2018-12-26 14:19:32 +0700

    Chapter12

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,63 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update] # Case (1)
+
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t "controllers.password_resets_controller.rightemail"
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "controllers.password_resets_controller.wrongemail"
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if params[:user][:password].empty? # Case (3)
+      @user.errors.add :password, t("controllers.password_resets_controller.emptypass")
+      render :edit
+    elsif @user.update_attributes(user_params) # Case (4)
+      log_in @user
+      @user.update_attribute :reset_digest, nil
+      flash[:success] = t "controllers.password_resets_controller.resetpass"
+      redirect_to @user
+    else
+      render :edit # Case (2)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
+
+  # Before filters
+
+  def get_user
+    @user = User.find_by email: params[:email]
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    return if @user&.activated? && @user.authenticated?(:reset, params[:id])
+
+    redirect_to root_url
+  end
+
+  # Checks expiration of reset token.
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = t "controllers.password_resets_controller.expiredpass"
+    redirect_to new_password_reset_url
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :load_user, only: %i(show correct_user edit update destroy)
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
-    flash[:danger] = t "controllers.users_controller.usernotfound"
-    redirect_to root_path
+  def index
+    @users = User.paginate(page: params[:page],
+      per_page: Settings.app.controllers.users_controller.perpage)
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -14,12 +18,29 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "controllers.users_controller.welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t "controllers.users_controller.checkemail"
+      redirect_to root_url
     else
       render :new
     end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = t "controllers.users_controller.updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.delete
+    flash[:success] = t "controllers.users_controller.deletedsuccess"
+    redirect_to users_url
   end
 
   private
@@ -27,5 +48,32 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "controllers.users_controller.errorlogin"
+    redirect_to login_url
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
