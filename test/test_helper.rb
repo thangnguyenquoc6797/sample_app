a7831c79b01ffc1965e7bc7eb4c076458683a68c
Thang6797 <thangnguyenquoc6797@gmail.com>
2018-12-27 15:34:08 +0700

    Update Chapter3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
+require File.expand_path("../config/environment", __dir__)
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   fixtures :all
+
+  # Add more helper methods to be used by all tests here...
 end
